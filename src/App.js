38abfdd661d9c38103a91ce740f85eb98d699fdb
chaitanya <chaitanyabatchu4@gmail.com>
2023-05-25 11:45:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+const url = "https://jsonplaceholder.typicode.com/users"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [userData, setuserData] = useState([])
+  const [loading, setloading] = useState(false)
+  const [isError, setErrMsg] = useState({ status: false, msg: "" })
+
+  const fetchUserData = async (url) => {
+    setloading(true)
+    setErrMsg({ status: false, msg: "" })
+    try {
+      const response = await fetch(url)
+      const data = await response.json();
+      console.log(data);
+      setuserData(data)
+      setloading(false)
+      setErrMsg({ status: false, msg: "" })
+    }
+    catch (error) {
+      setloading(false)
+      setErrMsg({ status: true, msg: "Something went wrong please try again" })
+    }
+  }
+  useEffect(() => {
+    fetchUserData(url);
+  }, [])
+  if (loading) {
+    return <div>
+      <h3>Loading....</h3>
     </div>
-  );
-}
+  }
+  if(isError?.status){
+    return <div>
+      {isError?.msg}
+    </div>
+  }
 
-export default App;
+  return (
+    <>
+      <div className='data'>
+        <h1>Data from Api</h1>
+        <ul>
+          {userData.map((eachUser) => {
+            const { id, name, email, phone } = eachUser;
+            return (
+              <div key={id}>
+                <li>{name}</li>
+                <li>{email}</li>
+                <li>{phone}</li>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
+    </>
+  )
+}
+export default App
